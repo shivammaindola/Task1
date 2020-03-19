287dea17e0e9294d2ebffe350603f0206eff7dd6
shivammaindola <shivammaindola07@gmail.com>
2020-03-19 21:23:39 +0530

    Added function

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -1,23 +1,15 @@
 package com.example.task1;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,34 +25,71 @@ public class MainActivity extends AppCompatActivity {
         Timestamp ts2 = Timestamp.valueOf("2020-09-01 03:01:15");
         Timestamp ts3 = Timestamp.valueOf("2019-09-01 03:01:15");
         Timestamp ts4 = Timestamp.valueOf("2022-09-01 03:01:15");
-        ChatModel chat1 = new ChatModel("hello shivam",1,21,31,ts1);
-        ChatModel chat2 = new ChatModel("hello ",2,22,32,ts2);
-        ChatModel chat3 = new ChatModel("hello1 ",3,23,33,ts3);
-        ChatModel chat4 = new ChatModel("hello2 ",4,24,34,ts4);
+        ChatModel chat1 = new ChatModel("hello shivam", 1, 21, 31, ts1);
+        ChatModel chat2 = new ChatModel("hello ", 2, 21, 31, ts2);
+        ChatModel chat3 = new ChatModel("hello1 ", 3, 23, null, ts3);
+        ChatModel chat4 = new ChatModel("hello2 ", 4, 23, null, ts4);
         messageset.add(chat1);
         messageset.add(chat2);
         messageset.add(chat3);
         messageset.add(chat4);
 
         //function
-        dosomething(messageset);
-    }
+        List<ChatModel> getlist= new ArrayList<>();
+        getlist=dosomething(messageset);
 
-    private void dosomething(List<ChatModel> messageset) {
-        //descending order of createdattime
-
-             Collections.sort(messageset, new Comparator<ChatModel>() {
-                 public int compare(ChatModel o1, ChatModel o2) {
-                     return o2.getCreatedAtTime().compareTo(o1.getCreatedAtTime());
-                 }
-             });
-
-        for(ChatModel model : messageset) {
-            {
-                Log.d("MYAPP", model.getMessage());
-                Log.d("MYAPP", model.getId().toString());
-                Log.d("MYAPP", model.getCreatedAtTime().toString());
-            }
+        //print list
+        for(ChatModel model : getlist)
+        {
+            Log.d("CheckList", model.getMessage());
         }
     }
+
+    private List<ChatModel> dosomething(List<ChatModel> messageset) {
+        //descending order of createdattime
+        Collections.sort(messageset, new Comparator<ChatModel>() {
+            public int compare(ChatModel o1, ChatModel o2) {
+                return o2.getCreatedAtTime().compareTo(o1.getCreatedAtTime());
+            }
+        });
+
+        //create a list with latest message
+        List<ChatModel> latestlist = new ArrayList<>();
+        latestlist.add(messageset.get(0));
+        for (int i = 1; i < messageset.size(); i++) {
+            Integer groupid = messageset.get(i).getGroupId();
+
+            if (groupid == null) {
+                //get userid
+                Integer userid = messageset.get(i).getUserId();
+                int check = 0;
+                for (int j = 0; j < latestlist.size(); j++) {
+                    if (userid.equals(latestlist.get(j).getUserId()) && latestlist.get(j).groupId == null) {
+                        check = 1;
+                        //do nothing
+                        break;
+                    }
+                }
+                if (check == 0) {
+                    //add this to latestlist
+                    latestlist.add(messageset.get(i));
+                }
+            } else {
+                int check = 0;
+                for (int j = 0; j < latestlist.size(); j++) {
+                    if (groupid.equals(latestlist.get(j).getGroupId())) {
+                        check = 1;
+                        break;
+                    }
+
+                }
+                if (check == 0) {
+                    latestlist.add(messageset.get(i));
+                }
+            }
+        }
+
+        return latestlist;
+    }
 }
+
